d5be37bc8efc4fa06fd785aa93330ba763a01e8a
redbean <redbeanmouse@gmail.com>
2015-10-06 00:16:16 +0900

    linked list
    No more than that,
    I need to done this assignment from 11 years ago.

---
+++ linkedlist.py
@@ -0,0 +1,36 @@
+#for my 10years old assignment that I did not finish.
+
+import sys, os
+
+class Node:
+     def __init__(self, data=None, next_node=None):
+        self.data = data
+        self.next = None
+
+    def get_data(self):
+
+    def get_next(self):
+
+    def set_next(self, new_next):
+     
+
+class LL:
+    def __init__(self, head=None):
+        self.head = head
+
+    def insert(self, data):
+        if self.ll_node.next == None:
+            print "first"
+
+    def size(self):
+
+    def search(self, data):
+
+    def delete(self, data):
+
+            
+        
+a = LL(123)
+a.first_node()
+#a.append(3)
+a.append(5)
